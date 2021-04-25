7319653a5effe53f75181449bfe073657f00d4af
Cathylee <oncwnuDeAXpe5fZxe34VedwUtGbA@git.weixin.qq.com>
2021-04-25 20:05:57 +0800

    ex02

---
+++ demo_02/src/Abstractbtn.java
@@ -0,0 +1,8 @@
+import javax.swing.JButton;
+
+
+public abstract class Abstractbtn extends JButton {
+    // Add serial Version UID
+    private static final long serialVersionUID = 1L;
+    public abstract void view();
+}
